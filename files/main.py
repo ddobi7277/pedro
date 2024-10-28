5a4e6679a2fe576d441e92f1ab63d1e1387c427f
ddobi7277 <ddobi7277@gmail.com>
2024-10-28 12:00:26 -0400

    Update main.py

--- files/main.py
+++ files/main.py
@@ -12,11 +12,11 @@ app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-origins= ["https://equity-limitation-terry-provider.trycloudflare.com/login","http://localhost:3000","https://equity-limitation-terry-provider.trycloudflare.com"]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= origins,
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
